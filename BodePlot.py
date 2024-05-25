d8bd0166fe8dd26c1b0763b55ecb28a82b494360
timpaasche <tim.paasche@outlook.de>
2024-05-25 10:14:19 +0200

    Refactor BodePlot.py and WokPlot.py for improved readability and maintainability

--- BodePlot.py
+++ BodePlot.py
@@ -13,18 +13,20 @@ omega_stop = 10000
 omega_steps = 1000
 
 # Definieren Sie die Übertragungsfunktion der Strecke
-k_s = 100
-t_s1 = 100
-t_s2 = 5
-t_s3 = 1
-strecke = k_s/((1+s*t_s3)*(1+t_s1*s)*(1+t_s2*s))
+k_mech = 5.21
+k_el = 6.25*10**-2
+k_s = k_mech*k_el
+k_r_db = 20
+k_r = -10**(k_r_db/20) 
+print(f"Verstärkung Regler: {k_r}")
 
-# Definieren Sie die Übertragungsfunktion des Reglers
-k_r_db = -60
-k_r = 10**(k_r_db/20) 
-t_r1 = t_s1
-regler = k_r * (1+t_r1*s) / s
-print(k_r)
+t_mech = 2.266*10**-2
+t_el = 5*10**-4
+t_v = t_mech
+t_d = 0.05*t_v
+
+strecke = k_s/((1+s*t_el)*(1+t_mech*s)*(1-t_mech*s))
+regler = (k_r * (1+t_v*s)) / (1+t_d*s)
 
 # System = Regler * Strecke
 system = regler * strecke
@@ -42,7 +44,7 @@ mag, phase = ct.bode(system,
                      Hz=False,
                      title=title)
 
-# Draw a line at -120 degrees in the phase diagram
-plt.axhline(y=-120, color='r', linestyle='--')
+# Draw a line at -180 degrees in the phase diagram
+#plt.axhline(y=-180, color='r', linestyle='--')
 # Zeigen Sie das Diagramm an
 plt.show()
--- WokPlot.py
+++ WokPlot.py
@@ -1,13 +1,18 @@
 import matplotlib.pyplot as plt
 import control as ct
+import numpy as np
 
 # Definieren Sie Ihr System
 s = ct.TransferFunction.s
-t_1 = 1
-t_2 = 0.1
-strecke = 1/(s*(1+t_1*s)*(1+t_2*s))
+t_1 = 15
+t_2 = 1.4
+k_1 = 1
+k_2 = 1
+strecke = 1/(s+1) 
+regler = 1/s
 
-# Wurzelortskurve plotten
-rlocus = ct.root_locus(strecke)
+
+
+ct.root_locus((regler * strecke))
 
 plt.show()
